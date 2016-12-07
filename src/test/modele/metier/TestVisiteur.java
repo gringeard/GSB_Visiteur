d77c3ccdebfd5b759518eddbe618ea9334464f4e
btssio <btssio@btssio-VirtualBox>
2016-12-07 17:19:12 +0100

    Création classe test métier visiteur

--- src/test/modele/metier/TestVisiteur.java
+++ src/test/modele/metier/TestVisiteur.java
@@ -19,7 +19,14 @@ public class TestVisiteur {
         date = new Date();
         System.out.println("\nTest n°1 : instanciation et accesseurs");
         visit = new Visiteur("a131" , "Villechalane", "Louis", "8 cours Lafontaine", "29000", "BREST", date,"", "Swiss Kane");
-        System.out.println(visit);
+        System.out.println("Nouveau visiteur : "+visit);
+        System.out.println("\nTest n°2 : mutateurs");
+        visit.setVis_adresse("56 boulevard de la Prairie aux Ducs");
+        visit.setVis_cp("44000");
+        visit.setVis_ville("Nantes");
+        System.out.println("Visiteur modifié : "+visit);
+        visit1 = new Visiteur("LEL",null, null, null, null, null, null, null, null);
+        System.out.println(visit1.equals(visit));
     }
     
 }
