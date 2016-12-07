2367ef63e2ac8360a661a181c30545099ca45739
btssio <btssio@btssio-VirtualBox>
2016-12-07 17:57:04 +0100

    test

---
+++ src/test/modele/dao/testDaoVisiteur.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.modele.dao;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author btssio
+ */
+public class testDaoVisiteur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+                java.sql.Connection cnx = null;
+
+                test0_Connexion();
+                System.out.println("Test0 effectué : connexion\n");
+        
+        
+    }
+
+    private static void test0_Connexion() {
+        
+    }
+    
+}
