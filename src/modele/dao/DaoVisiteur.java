7d571817b94636fa684fc7dc0ddbc88fe3368826
btssio <btssio@btssio-VirtualBox>
2017-03-01 17:52:12 +0100

    DaoVisiteur ajout

--- src/modele/dao/DaoVisiteur.java
+++ src/modele/dao/DaoVisiteur.java
@@ -19,24 +19,25 @@ public class DaoVisiteur {
     
     /**
      * Extraction d'un visiteur, sur son identifiant
-     * @param idVisiteur identifiant
+     * @param matriculeVisiteur identifiant
      * @return objet Visiteur
      * @throws SQLException 
      */
-    public static Visiteur selectOne(String idVisiteur) throws SQLException {
-        Visiteur unVisiteur = null;
+    public static Visiteur selectOneByID(String matricule) throws SQLException {
         ResultSet rs;
         PreparedStatement pstmt;
         Jdbc jdbc = Jdbc.getInstance();
+        
         // préparer la requête
-        String requete = "SELECT * FROM ADRESSE WHERE ID= ?";
+        String requete = "SELECT * FROM VISITEUR WHERE VIS_MATRICULE = ?";
         pstmt = jdbc.getConnexion().prepareStatement(requete);
-        pstmt.setString(1, idVisiteur);
+        pstmt.setString(1, matricule);
         rs = pstmt.executeQuery();
+        Visiteur unVisiteur = null;
         if (rs.next()) {
             String id = rs.getString("VIS_MATRICULE");
             String nom = rs.getString("VIS_NOM");
-            String prenom = rs.getString("Vis_PRENOM");
+            String prenom = rs.getString("VIS_PRENOM");
             String adresse = rs.getString("VIS_ADRESSE");
             String cp = rs.getString("VIS_CP");
             String ville = rs.getString("VIS_VILLE");
--- src/test/modele/dao/testDaoVisiteur.java
+++ src/test/modele/dao/testDaoVisiteur.java
@@ -6,30 +6,45 @@
 package test.modele.dao;
 
 import java.sql.SQLException;
+import modele.metier.Visiteur;
 import modele.dao.Jdbc;
+import modele.dao.DaoVisiteur;
 
 /**
  *
  * @author btssio
  */
+import java.sql.Connection;
 public class testDaoVisiteur {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         // TODO code application logic here
         
                 java.sql.Connection cnx = null;
 
                 test0_Connexion();
-                System.out.println("Test0 effectué : connexion\n");
-        
+                System.out.println("Test1 effectué : connexion\n");
+                test1_GetOneVisiteur();
+                System.out.println("Test2 effectué : get one\n");
         
     }
 
-    private static void test0_Connexion() {
-        Jdbc.creer("oracle.jdbc.driver.OracleDriver", "jdbc:oracle:thin:", "@localhost:1521:XE", "", "GSB", "gsb");
+    private static void test0_Connexion() throws SQLException, ClassNotFoundException {
+        Jdbc.creer("oracle.jdbc.driver.OracleDriver", "jdbc:oracle:thin:", "@localhost:1521:XE", "", "gsb", "gsb");
+        Jdbc.getInstance().connecter();
+        Connection cnx = Jdbc.getInstance().getConnexion();
+        
+        System.out.println(cnx);
+    }
+    
+    private static void test1_GetOneVisiteur() throws SQLException {
+        Visiteur test;
+        String matricule = "a131";
+        test = DaoVisiteur.selectOneByID(matricule);
+        System.out.println(test);
     }
     
 }
