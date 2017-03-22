c8c1a541cb45fa7fed33e336d72312950aff6e85
btssio <btssio@btssio-VirtualBox>
2017-03-22 14:24:17 +0100

    ajout de commentaires dans les contrôleurs

--- src/controleur/CtrlPrincipal.java
+++ src/controleur/CtrlPrincipal.java
@@ -2,15 +2,24 @@ package controleur;
 
 import javax.swing.JOptionPane;
 
+/**
+ * 
+ * @author btssio
+ */
 public class CtrlPrincipal {
 
     CtrlVisiteur ctrlVisiteur;
 
+    /**
+     * fonction permettant d'afficher la vue associée au contrôleur Visiteur
+     */
     public void afficherVisiteur() {
         this.ctrlVisiteur.getVue().setVisible(true);
     }
 
-
+    /**
+     * fonction permettant de fermer l'application
+     */
     public void quitterApplication() {
         // Confirmer avant de quitter
         int rep = JOptionPane.showConfirmDialog(null, "Quitter l'application\nEtes-vous sûr(e) ?", "GSB", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
@@ -20,10 +29,20 @@ public class CtrlPrincipal {
         }
     }
 
+    /**
+     * fonction permettant de retourner le contrôleur Visiteur
+     * 
+     * @return 
+     */
     public CtrlVisiteur getCtrlVisiteur() {
         return ctrlVisiteur;
     }
 
+    /**
+     * fonction permettant de modifier le contrôleur Visiteur
+     * 
+     * @param ctrlVisiteur 
+     */
     public void setCtrlVisiteur(CtrlVisiteur ctrlVisiteur) {
         this.ctrlVisiteur = ctrlVisiteur;
     }    
